9ff7a28b95aac3b3eb60759291c6a8b410260e06
Puskar Thapa Magar <puskarmagar0215@gmail.com>
2026-05-27 22:17:45 +0545

    added the snake and ladder

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Main{
 
     //snake and ladders position
@@ -5,7 +7,30 @@ public class Main{
     static int[] ladders = new int[101];
 
     //setup snake and ladders
-    public stati c void setupBoard(){
+    public static void setupBoard(){
+        //snakes
+        snake[99] = 54;
+        snake[70] = 55;
+        snake[52] = 42;
+        snake[25] = 2;
+
+        //ladders
+        ladders[6] = 25;
+        ladders[11] = 40;
+        ladders[60] = 85;
+        ladders[46] = 90;
+    }
+
+    //method to roll the dice
+    public static int rollDice(){
+        Random random = new Random();
+
+        return random.nextInt(6) + 1;
+    }
+
+    //main method
+    public static void main(String[] args) {
 
     }
+
 }
