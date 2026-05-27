7ffd3a15f0315e47df3efc5d313f4c6f0bcee48a
Puskar Thapa Magar <puskarmagar0215@gmail.com>
2026-05-27 22:29:14 +0545

    added player 1 conditions

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main{
 
@@ -30,7 +31,54 @@ public class Main{
 
     //main method
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        setupBoard();
+
+        int player1 = 0;
+        int player2 = 0;
+
+        boolean gameOver = false;
+
+        System.out.println("===== Snake and Ladder Game =====");
+
+        while (!gameOver) {
+
+            //player 1 turn
+            System.out.println("\nPlayer 1 Press Enter to Roll Dice");
+            sc.nextLine();
+            int dice = rollDice();
+
+            System.out.println("Player 1 rolled: " + dice);
+
+            player1 += dice;
+
+            //check board limit
+            if (player1 > 100) {
+                player1 -= dice;
+            }
+
+            //check snake
+            if (snake[player1] != 0){
+                System.out.println("Oops! Bitten by Snake!");
+                player1 = snake[player1];
+            }
+
+            //Check ladder
+            else if (ladders[player1] != 0) {
+                System.out.println("Great! Climbed a Ladder!");
+                player1 = ladders[player1];
+            }
+            System.out.println("Player 1 Position: " + player1);
+
+            if (player1 == 100){
+                System.out.println("\nPlayer 1 Wins!");
+                break;
+            }
+
+            //Player 2 turn
+
+        }
 
     }
-
 }
