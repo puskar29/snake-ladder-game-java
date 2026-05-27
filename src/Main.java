eae586173c1653805337dd5d5eaee1cc43fff22b
Puskar Thapa Magar <puskarmagar0215@gmail.com>
2026-05-27 22:12:59 +0545

    making game

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main{
+
+    //snake and ladders position
+    static int[] snake = new int[101];
+    static int[] ladders = new int[101];
+
+    //setup snake and ladders
+    public stati c void setupBoard(){
+
+    }
+}
